f39b3103072ee56f1cb6878e92ad95237a5b925e
Дмитрий <50549999+MinusD@users.noreply.github.com>
2022-04-04 04:34:10 +0300

    Done A101

--- task_A101.py
+++ task_A101.py
@@ -9,16 +9,36 @@ import traceback
 
 
 def balanced_num(number):
-    # Тело функции
-    return True
+    number_str = str(number)  # Преобразовываем число в строку
+    if len(number_str) <= 1:  # Если длина 1, то сразу возращаем true
+        return True
+    else:
+        middle = len(number_str) // 2  # Ищём середину
+
+        # У нас в любом случае массивы разделятся на равные, если количество элементов
+        # было не чётное, то средний элемент просто не будет учитываться
+        a1 = number_str[:middle]  # От начала до середины
+        if len(number_str) % 2 == 1:  # Если число чётное, то используем все элементы, если нет, игнорируем центральный
+            a2 = number_str[(middle + 1):]  # Не учитываем центральный элемент
+        else:
+            a2 = number_str[middle:]  # Учитываем все
+        sum1 = 0
+        sum2 = 0
+        for i in range(len(a1)):
+            sum1 += int(a1[i])
+            sum2 += int(a2[i])
+        if sum1 == sum2:
+            return True
+        else:
+            return False
 
 
 # Тесты
 try:
-    assert balanced_num(13) == True
+    assert balanced_num(13) == False
     assert balanced_num(0) == True
     assert balanced_num(295591) == False
-    assert balanced_num(56239814) == True
+    assert balanced_num(56239814) == False
     assert balanced_num(1230987) == False
 except AssertionError:
     print("TEST ERROR")
